90d564b30c84766ee77bf34d8824ceb6010ba58c
runneradmin <runneradmin@fv-az71-323.bxmsiuwdzbaetnvmcmvhpox2jg.jx.internal.cloudapp.net>
2022-05-18 01:49:16 +0900

    2022-05-18T01:49:16.736+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Ypcysurml ypcysurml = new Ypcysurml();
+		ypcysurml.speak();
+
+
 		Xgpbsvj xgpbsvj = new Xgpbsvj();
 		xgpbsvj.speak();
 
---
+++ src/project/Ypcysurml.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Ypcysurml {
+	String message = "Ypcysurml";
+	
+	public Ypcysurml() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
