de711dcde12f8411371340140cdcd9e3ff88aa93
runneradmin <runneradmin@fv-az455-178.q1uzzir4eycuxanwfv2w1rtuxg.dx.internal.cloudapp.net>
2022-05-17 21:35:50 +0900

    2022-05-17T21:35:50.475+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Xgpbsvj xgpbsvj = new Xgpbsvj();
+		xgpbsvj.speak();
+
+
 		Xhgrxahcvt xhgrxahcvt = new Xhgrxahcvt();
 		xhgrxahcvt.speak();
 
---
+++ src/project/Xgpbsvj.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Xgpbsvj {
+	String message = "Xgpbsvj";
+	
+	public Xgpbsvj() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
