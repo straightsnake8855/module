eb21b8a8a1a1390b4b18ecf7e0ac007c3ba88609
runneradmin <runneradmin@fv-az171-18.ktbnycnmqcfudbavfw4edtkmzc.xx.internal.cloudapp.net>
2022-05-19 06:02:07 +0900

    2022-05-19T06:02:07.560+09:00

---
+++ src/project/Ksfvryefyl.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Ksfvryefyl {
+	String message = "Ksfvryefyl";
+	
+	public Ksfvryefyl() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Ksfvryefyl ksfvryefyl = new Ksfvryefyl();
+		ksfvryefyl.speak();
+
+
 		Lnrirng lnrirng = new Lnrirng();
 		lnrirng.speak();
 
