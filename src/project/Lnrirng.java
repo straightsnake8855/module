eef754d42382ac3171a65376eebc53dd28ceb2ff
runneradmin <runneradmin@fv-az455-178.q1uzzir4eycuxanwfv2w1rtuxg.dx.internal.cloudapp.net>
2022-05-19 02:39:08 +0900

    2022-05-19T02:39:08.791+09:00

---
+++ src/project/Lnrirng.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Lnrirng {
+	String message = "Lnrirng";
+	
+	public Lnrirng() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Lnrirng lnrirng = new Lnrirng();
+		lnrirng.speak();
+
+
 		Akfcgnaq akfcgnaq = new Akfcgnaq();
 		akfcgnaq.speak();
 
