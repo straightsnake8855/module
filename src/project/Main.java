72ed828cfe751d56d15be4d6447839f20b8ae39d
runneradmin <runneradmin@fv-az177-340.fbf4kbtgkivung042urq0hmtuh.cx.internal.cloudapp.net>
2022-05-21 17:16:21 +0900

    2022-05-21T17:16:21.271+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Tgusvh tgusvh = new Tgusvh();
+		tgusvh.speak();
+
+
 		Lutne lutne = new Lutne();
 		lutne.speak();
 
---
+++ src/project/Tgusvh.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Tgusvh {
+	String message = "Tgusvh";
+	
+	public Tgusvh() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
