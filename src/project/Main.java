5c8be7e84abac74da788a069f6903380c7a909a8
runneradmin <runneradmin@fv-az455-848.q1uzzir4eycuxanwfv2w1rtuxg.dx.internal.cloudapp.net>
2022-05-23 21:07:17 +0900

    2022-05-23T21:07:17.922+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Pniehc pniehc = new Pniehc();
+		pniehc.speak();
+
+
 		Cloth cloth = new Cloth();
 		cloth.speak();
 
---
+++ src/project/Pniehc.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Pniehc {
+	String message = "Pniehc";
+	
+	public Pniehc() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
