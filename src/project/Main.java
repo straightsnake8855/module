f9786962d0f1b839dab4ced8c5ce9e86db02a792
runneradmin <runneradmin@fv-az56-419.0tmuwhocqiwuhlyrr5sn0m1sxb.dx.internal.cloudapp.net>
2022-05-24 10:09:18 +0900

    2022-05-24T10:09:18.879+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Xaiai xaiai = new Xaiai();
+		xaiai.speak();
+
+
 		Pniehc pniehc = new Pniehc();
 		pniehc.speak();
 
---
+++ src/project/Xaiai.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Xaiai {
+	String message = "Xaiai";
+	
+	public Xaiai() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
