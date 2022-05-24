a3597b2d454e174b2622f5935a1809d075ef0e2f
runneradmin <runneradmin@fv-az56-341.0tmuwhocqiwuhlyrr5sn0m1sxb.dx.internal.cloudapp.net>
2022-05-25 05:52:54 +0900

    2022-05-25T05:52:54.659+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Yuxcrp yuxcrp = new Yuxcrp();
+		yuxcrp.speak();
+
+
 		Jlcptqfp jlcptqfp = new Jlcptqfp();
 		jlcptqfp.speak();
 
---
+++ src/project/Yuxcrp.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Yuxcrp {
+	String message = "Yuxcrp";
+	
+	public Yuxcrp() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
