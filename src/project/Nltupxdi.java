f961a6f4c1f961d45df2d6c8cc7aa00d617ac453
runneradmin <runneradmin@fv-az455-268.q1uzzir4eycuxanwfv2w1rtuxg.dx.internal.cloudapp.net>
2022-05-27 09:24:29 +0900

    2022-05-27T09:24:29.261+09:00

---
+++ src/project/Nltupxdi.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Nltupxdi {
+	String message = "Nltupxdi";
+	
+	public Nltupxdi() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
