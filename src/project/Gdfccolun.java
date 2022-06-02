e22c35d345936b2fbd6cca95dacb7af8f7776394
runneradmin <runneradmin@fv-az207-708.5iaplmtnmcou5pxwvbplryh23g.dx.internal.cloudapp.net>
2022-06-02 18:55:21 +0900

    2022-06-02T18:55:21.469+09:00

---
+++ src/project/Gdfccolun.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Gdfccolun {
+	String message = "Gdfccolun";
+	
+	public Gdfccolun() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
