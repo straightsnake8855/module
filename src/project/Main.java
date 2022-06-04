6b057fbc66c251d29c79e2a314aa62cd658f7605
runneradmin <runneradmin@fv-az8-328.i4j43oenqpxu3ba2y5r2bpi2ob.gx.internal.cloudapp.net>
2022-06-05 06:23:54 +0900

    2022-06-05T06:23:54.823+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Tubwkqxehv tubwkqxehv = new Tubwkqxehv();
+		tubwkqxehv.speak();
+
+
 		Wymhwjtkbq wymhwjtkbq = new Wymhwjtkbq();
 		wymhwjtkbq.speak();
 
---
+++ src/project/Tubwkqxehv.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Tubwkqxehv {
+	String message = "Tubwkqxehv";
+	
+	public Tubwkqxehv() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
