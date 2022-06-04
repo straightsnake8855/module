8ab6dbd15e1dc94de2a2c29eeb5a016035bb5d67
runneradmin <runneradmin@fv-az8-328.i4j43oenqpxu3ba2y5r2bpi2ob.gx.internal.cloudapp.net>
2022-06-04 23:45:40 +0900

    2022-06-04T23:45:39.975+09:00

---
+++ src/project/Dpida.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Dpida {
+	String message = "Dpida";
+	
+	public Dpida() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Dpida dpida = new Dpida();
+		dpida.speak();
+
+
 		Rgtjqf rgtjqf = new Rgtjqf();
 		rgtjqf.speak();
 
