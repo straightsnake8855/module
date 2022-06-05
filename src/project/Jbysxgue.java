819c2ac2b690ac4ca2f4382b5bc201b7a3cf41db
runneradmin <runneradmin@fv-az158-67.xgx2um1fv31endmuljrapny3if.bx.internal.cloudapp.net>
2022-06-05 15:00:15 +0900

    2022-06-05T15:00:15.846+09:00

---
+++ src/project/Jbysxgue.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Jbysxgue {
+	String message = "Jbysxgue";
+	
+	public Jbysxgue() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Jbysxgue jbysxgue = new Jbysxgue();
+		jbysxgue.speak();
+
+
 		Llqbcfsm llqbcfsm = new Llqbcfsm();
 		llqbcfsm.speak();
 
