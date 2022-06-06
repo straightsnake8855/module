a98720deb3d5652e2746e47415ae61eba7f81c15
runneradmin <runneradmin@fv-az56-207.0tmuwhocqiwuhlyrr5sn0m1sxb.dx.internal.cloudapp.net>
2022-06-06 16:03:40 +0900

    2022-06-06T16:03:39.959+09:00

---
+++ src/project/Apmyj.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Apmyj {
+	String message = "Apmyj";
+	
+	public Apmyj() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Apmyj apmyj = new Apmyj();
+		apmyj.speak();
+
+
 		Auufok auufok = new Auufok();
 		auufok.speak();
 
