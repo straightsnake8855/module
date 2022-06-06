752462b866d05c3eabffda249ce8362c9c64dd8c
runneradmin <runneradmin@fv-az108-11.wlxd5ieacocexbvd32ck1xvtjd.gx.internal.cloudapp.net>
2022-06-06 17:58:41 +0900

    2022-06-06T17:58:41.015+09:00

---
+++ src/project/Okihpmtk.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Okihpmtk {
+	String message = "Okihpmtk";
+	
+	public Okihpmtk() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
