eeb9aab6fd41b9ff1a6c5b87f5a92da2e15a7488
runneradmin <runneradmin@fv-az455-861.q1uzzir4eycuxanwfv2w1rtuxg.dx.internal.cloudapp.net>
2022-06-08 04:11:18 +0900

    2022-06-08T04:11:18.097+09:00

---
+++ src/project/Geuqaxqweu.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Geuqaxqweu {
+	String message = "Geuqaxqweu";
+	
+	public Geuqaxqweu() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Geuqaxqweu geuqaxqweu = new Geuqaxqweu();
+		geuqaxqweu.speak();
+
+
 		Hcgcoeb hcgcoeb = new Hcgcoeb();
 		hcgcoeb.speak();
 
