d2f412cb82814367bb951a50742c53e49e56bc62
runneradmin <runneradmin@fv-az71-85.bxmsiuwdzbaetnvmcmvhpox2jg.jx.internal.cloudapp.net>
2022-06-09 17:12:54 +0900

    2022-06-09T17:12:54.732+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Vbeogkx vbeogkx = new Vbeogkx();
+		vbeogkx.speak();
+
+
 		Ybgprbm ybgprbm = new Ybgprbm();
 		ybgprbm.speak();
 
---
+++ src/project/Vbeogkx.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Vbeogkx {
+	String message = "Vbeogkx";
+	
+	public Vbeogkx() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
