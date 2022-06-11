8fea127a45cb80b5e71c0c2b3f963fbc63636431
runneradmin <runneradmin@fv-az158-297.xgx2um1fv31endmuljrapny3if.bx.internal.cloudapp.net>
2022-06-11 19:06:24 +0900

    2022-06-11T19:06:23.980+09:00

---
+++ src/project/Lrojrscarl.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Lrojrscarl {
+	String message = "Lrojrscarl";
+	
+	public Lrojrscarl() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Lrojrscarl lrojrscarl = new Lrojrscarl();
+		lrojrscarl.speak();
+
+
 		Arllsegh arllsegh = new Arllsegh();
 		arllsegh.speak();
 
