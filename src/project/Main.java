f0f8d89f5829324e6cf2c19af960e5a996c3d6f9
runneradmin <runneradmin@fv-az276-734.ahqxcs3jn4cepgq4141lq0tbta.jx.internal.cloudapp.net>
2022-06-11 10:39:39 +0900

    2022-06-11T10:39:39.140+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Wkgwrnsyb wkgwrnsyb = new Wkgwrnsyb();
+		wkgwrnsyb.speak();
+
+
 		Urlvxwluks urlvxwluks = new Urlvxwluks();
 		urlvxwluks.speak();
 
---
+++ src/project/Wkgwrnsyb.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Wkgwrnsyb {
+	String message = "Wkgwrnsyb";
+	
+	public Wkgwrnsyb() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
