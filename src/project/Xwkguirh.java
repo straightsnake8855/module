fb569ede31196a610f6276cb851b452abefe1e94
runneradmin <runneradmin@fv-az15-131.vmnhtn5vcynurpvc1b4hs5ikkf.cx.internal.cloudapp.net>
2022-06-13 10:47:02 +0900

    2022-06-13T10:47:02.263+09:00

---
+++ src/project/Xwkguirh.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Xwkguirh {
+	String message = "Xwkguirh";
+	
+	public Xwkguirh() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
