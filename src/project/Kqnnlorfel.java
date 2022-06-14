eb99b78d3590b3555a0b6a1812276d9dbc25c8cf
runneradmin <runneradmin@fv-az320-154.et524aexjeeuxj4vvr0zf1myma.dx.internal.cloudapp.net>
2022-06-14 17:00:42 +0900

    2022-06-14T17:00:42.650+09:00

---
+++ src/project/Kqnnlorfel.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Kqnnlorfel {
+	String message = "Kqnnlorfel";
+	
+	public Kqnnlorfel() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Kqnnlorfel kqnnlorfel = new Kqnnlorfel();
+		kqnnlorfel.speak();
+
+
 		Xwkguirh xwkguirh = new Xwkguirh();
 		xwkguirh.speak();
 
