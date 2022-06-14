a8d77040535f7654f43662654fab1103e71328b8
runneradmin <runneradmin@fv-az56-244.0tmuwhocqiwuhlyrr5sn0m1sxb.dx.internal.cloudapp.net>
2022-06-15 06:54:45 +0900

    2022-06-15T06:54:45.839+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Vnavjwgqm vnavjwgqm = new Vnavjwgqm();
+		vnavjwgqm.speak();
+
+
 		Kqnnlorfel kqnnlorfel = new Kqnnlorfel();
 		kqnnlorfel.speak();
 
---
+++ src/project/Vnavjwgqm.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Vnavjwgqm {
+	String message = "Vnavjwgqm";
+	
+	public Vnavjwgqm() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
