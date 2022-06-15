cb363d2770e428c111d5134656d756cf1554ea94
runneradmin <runneradmin@fv-az455-439.q1uzzir4eycuxanwfv2w1rtuxg.dx.internal.cloudapp.net>
2022-06-15 23:01:26 +0900

    2022-06-15T23:01:26.289+09:00

---
+++ src/project/Ggdjrlauqb.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Ggdjrlauqb {
+	String message = "Ggdjrlauqb";
+	
+	public Ggdjrlauqb() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
