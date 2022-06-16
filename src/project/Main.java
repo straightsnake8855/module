587b5865282a9e907e79a08473b72bd0e94d4514
runneradmin <runneradmin@fv-az28-185.z0kwderdcn0ufmexdgqaii5tvd.gx.internal.cloudapp.net>
2022-06-16 23:48:47 +0900

    2022-06-16T23:48:47.927+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Mbknafptew mbknafptew = new Mbknafptew();
+		mbknafptew.speak();
+
+
 		Kxptuveba kxptuveba = new Kxptuveba();
 		kxptuveba.speak();
 
---
+++ src/project/Mbknafptew.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Mbknafptew {
+	String message = "Mbknafptew";
+	
+	public Mbknafptew() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
