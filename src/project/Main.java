18df543de941201e27c1325d14be265bb2a9dd46
runneradmin <runneradmin@fv-az455-27.q1uzzir4eycuxanwfv2w1rtuxg.dx.internal.cloudapp.net>
2022-06-18 06:45:49 +0900

    2022-06-18T06:45:49.279+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Snvbyh snvbyh = new Snvbyh();
+		snvbyh.speak();
+
+
 		Irhro irhro = new Irhro();
 		irhro.speak();
 
---
+++ src/project/Snvbyh.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Snvbyh {
+	String message = "Snvbyh";
+	
+	public Snvbyh() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
