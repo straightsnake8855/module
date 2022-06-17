caf3a4e528abcc9595ae268266368d6c4490c60b
runneradmin <runneradmin@fv-az195-553.yllzni5slbnuzhc1hkjsfo1d4g.jx.internal.cloudapp.net>
2022-06-17 17:04:57 +0900

    2022-06-17T17:04:57.751+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Rjugku rjugku = new Rjugku();
+		rjugku.speak();
+
+
 		Mbknafptew mbknafptew = new Mbknafptew();
 		mbknafptew.speak();
 
---
+++ src/project/Rjugku.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Rjugku {
+	String message = "Rjugku";
+	
+	public Rjugku() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
