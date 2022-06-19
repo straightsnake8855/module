b124e5a5fb1707c6c22cfe9e10d2ed687c5e7c81
runneradmin <runneradmin@fv-az455-27.q1uzzir4eycuxanwfv2w1rtuxg.dx.internal.cloudapp.net>
2022-06-19 10:03:05 +0900

    2022-06-19T10:03:05.868+09:00

---
+++ src/project/Fypbrvmpuw.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Fypbrvmpuw {
+	String message = "Fypbrvmpuw";
+	
+	public Fypbrvmpuw() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
