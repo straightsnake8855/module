27437dabb5034a38424c71bf7c5a203602b02b5a
runneradmin <runneradmin@fv-az177-838.fbf4kbtgkivung042urq0hmtuh.cx.internal.cloudapp.net>
2022-06-19 16:51:29 +0900

    2022-06-19T16:51:28.992+09:00

---
+++ src/project/Liekqt.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Liekqt {
+	String message = "Liekqt";
+	
+	public Liekqt() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Liekqt liekqt = new Liekqt();
+		liekqt.speak();
+
+
 		Fypbrvmpuw fypbrvmpuw = new Fypbrvmpuw();
 		fypbrvmpuw.speak();
 
