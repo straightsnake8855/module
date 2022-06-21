11ddbb56837d1af96757d84cbdb310978008d2eb
runneradmin <runneradmin@fv-az56-553.0tmuwhocqiwuhlyrr5sn0m1sxb.dx.internal.cloudapp.net>
2022-06-21 21:54:32 +0900

    2022-06-21T21:54:32.125+09:00

---
+++ src/project/Lqetqm.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Lqetqm {
+	String message = "Lqetqm";
+	
+	public Lqetqm() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Lqetqm lqetqm = new Lqetqm();
+		lqetqm.speak();
+
+
 		Kefostj kefostj = new Kefostj();
 		kefostj.speak();
 
