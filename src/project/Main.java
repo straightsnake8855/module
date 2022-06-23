ec75f347c636e90f0adb1438a05338af8cb72695
runneradmin <runneradmin@fv-az320-565.et524aexjeeuxj4vvr0zf1myma.dx.internal.cloudapp.net>
2022-06-24 01:44:20 +0900

    2022-06-24T01:44:20.958+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Ovfdqvrfky ovfdqvrfky = new Ovfdqvrfky();
+		ovfdqvrfky.speak();
+
+
 		Nshsb nshsb = new Nshsb();
 		nshsb.speak();
 
---
+++ src/project/Ovfdqvrfky.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Ovfdqvrfky {
+	String message = "Ovfdqvrfky";
+	
+	public Ovfdqvrfky() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
