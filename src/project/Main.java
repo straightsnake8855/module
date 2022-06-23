c7d76dbc657954e23e6a3612976ec71aca4ed0bd
runneradmin <runneradmin@fv-az171-154.ktbnycnmqcfudbavfw4edtkmzc.xx.internal.cloudapp.net>
2022-06-23 15:29:11 +0900

    2022-06-23T15:29:11.605+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Nshsb nshsb = new Nshsb();
+		nshsb.speak();
+
+
 		Miarlcryyy miarlcryyy = new Miarlcryyy();
 		miarlcryyy.speak();
 
---
+++ src/project/Nshsb.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Nshsb {
+	String message = "Nshsb";
+	
+	public Nshsb() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
