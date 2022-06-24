0d01e40c0b5a828f6437d387604e8a1ab37fef1f
runneradmin <runneradmin@fv-az455-620.q1uzzir4eycuxanwfv2w1rtuxg.dx.internal.cloudapp.net>
2022-06-24 15:16:37 +0900

    2022-06-24T15:16:37.348+09:00

---
+++ src/project/Dqgbo.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Dqgbo {
+	String message = "Dqgbo";
+	
+	public Dqgbo() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Dqgbo dqgbo = new Dqgbo();
+		dqgbo.speak();
+
+
 		Opjjxrpu opjjxrpu = new Opjjxrpu();
 		opjjxrpu.speak();
 
