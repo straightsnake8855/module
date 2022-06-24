f99c5d8ee2eab21962f71602da0484701f8259d9
runneradmin <runneradmin@fv-az108-91.wlxd5ieacocexbvd32ck1xvtjd.gx.internal.cloudapp.net>
2022-06-24 11:16:52 +0900

    2022-06-24T11:16:52.123+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Opjjxrpu opjjxrpu = new Opjjxrpu();
+		opjjxrpu.speak();
+
+
 		Ovfdqvrfky ovfdqvrfky = new Ovfdqvrfky();
 		ovfdqvrfky.speak();
 
---
+++ src/project/Opjjxrpu.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Opjjxrpu {
+	String message = "Opjjxrpu";
+	
+	public Opjjxrpu() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
