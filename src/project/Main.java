a8cdb173ab04acb51275c1350b880c725983a828
runneradmin <runneradmin@fv-az30-172.hsehkm54aiceteolt3vuaxdlqa.gx.internal.cloudapp.net>
2022-06-25 22:07:15 +0900

    2022-06-25T22:07:15.508+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Ouugpga ouugpga = new Ouugpga();
+		ouugpga.speak();
+
+
 		Dqgbo dqgbo = new Dqgbo();
 		dqgbo.speak();
 
---
+++ src/project/Ouugpga.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Ouugpga {
+	String message = "Ouugpga";
+	
+	public Ouugpga() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
