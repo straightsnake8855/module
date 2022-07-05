5e1430196b518586e4c6db4b57937ca615bf2be6
runneradmin <runneradmin@fv-az204-928.fvxkxpgotcoevm2hj51qsqoxsc.jx.internal.cloudapp.net>
2022-07-06 05:31:06 +0900

    2022-07-06T05:31:06.077+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Yuyxjmix yuyxjmix = new Yuyxjmix();
+		yuyxjmix.speak();
+
+
 		Scvoxcb scvoxcb = new Scvoxcb();
 		scvoxcb.speak();
 
---
+++ src/project/Yuyxjmix.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Yuyxjmix {
+	String message = "Yuyxjmix";
+	
+	public Yuyxjmix() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
