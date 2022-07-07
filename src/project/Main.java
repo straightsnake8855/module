74fde15fcca81904b9ba636bbbccf5b44c8f89e1
runneradmin <runneradmin@fv-az171-438.ktbnycnmqcfudbavfw4edtkmzc.xx.internal.cloudapp.net>
2022-07-07 10:01:53 +0900

    2022-07-07T10:01:53.145+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Spkmnbecs spkmnbecs = new Spkmnbecs();
+		spkmnbecs.speak();
+
+
 		Bkqmvdxogl bkqmvdxogl = new Bkqmvdxogl();
 		bkqmvdxogl.speak();
 
---
+++ src/project/Spkmnbecs.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Spkmnbecs {
+	String message = "Spkmnbecs";
+	
+	public Spkmnbecs() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
