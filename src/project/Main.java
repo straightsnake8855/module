d7bbf0a53e8a2fd59c3d30b12a092e2780b34555
runneradmin <runneradmin@fv-az320-768.et524aexjeeuxj4vvr0zf1myma.dx.internal.cloudapp.net>
2022-07-12 05:05:33 +0900

    2022-07-12T05:05:33.592+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Xtvjq xtvjq = new Xtvjq();
+		xtvjq.speak();
+
+
 		Bcnhiua bcnhiua = new Bcnhiua();
 		bcnhiua.speak();
 
---
+++ src/project/Xtvjq.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Xtvjq {
+	String message = "Xtvjq";
+	
+	public Xtvjq() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
