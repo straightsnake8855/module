b7a04d107b9b06a027d5515af82d25f417d1068e
runneradmin <runneradmin@fv-az30-652.hsehkm54aiceteolt3vuaxdlqa.gx.internal.cloudapp.net>
2022-07-14 09:20:24 +0900

    2022-07-14T09:20:24.366+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Tiyef tiyef = new Tiyef();
+		tiyef.speak();
+
+
 		Xtvjq xtvjq = new Xtvjq();
 		xtvjq.speak();
 
---
+++ src/project/Tiyef.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Tiyef {
+	String message = "Tiyef";
+	
+	public Tiyef() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
