5300994bab1a9f568338cafae2bb7c447243c3e6
runneradmin <runneradmin@fv-az204-375.fvxkxpgotcoevm2hj51qsqoxsc.jx.internal.cloudapp.net>
2022-07-19 13:23:41 +0900

    2022-07-19T13:23:41.279+09:00

--- src/project/Main.java
+++ src/project/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		System.out.println("HelloWorld");
 		// START
 
+		Ossypdfsdd ossypdfsdd = new Ossypdfsdd();
+		ossypdfsdd.speak();
+
+
 		Tiyef tiyef = new Tiyef();
 		tiyef.speak();
 
---
+++ src/project/Ossypdfsdd.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Ossypdfsdd {
+	String message = "Ossypdfsdd";
+	
+	public Ossypdfsdd() {
+		super();
+	}
+	
+	public void speak() {
+		System.out.println(message);
+	}
+}
